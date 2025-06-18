7da8d1f6934c3587ebf66447bea9c92906c523e1
ShivamP2E <144093375+ShivamP2E@users.noreply.github.com>
2025-06-19 02:19:58 +0530

    get request

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,33 @@ const app = express();
 const PORT = 8000;
 
 //Routes
-app.get("/users", (req,res)=>{
+app.get("/api/users", (req,res)=>{
     return res.json(users);
 })
+app.get("/users", (req,res)=>{
+    const html = 
+    `<ul>
+    ${users.map((user)=>
+        `<li>${user.first_name}</li>`
+    ).join("")} 
+    </ul>
+    `;
+    res.send(html);
+})
+app.get("/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const searchUser = users.find(user=> user.id == id)
+    if(searchUser)
+    {
+        res.json(searchUser)
+    }
+    else{
+        res.status(404).send("User not found")
+    }
 
+})
+
+
+ 
 
 app.listen(PORT,()=> console.log("app is launched"))
